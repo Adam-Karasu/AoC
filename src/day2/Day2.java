0d537b268322be007439dd604af54f0f43eef8b7
Murad Karasu <muradkarasu@gmail.com>
2016-02-12 12:15:45 +0000

    Day2 Part1 complete

--- src/day2/Day2.java
+++ src/day2/Day2.java
@@ -1,7 +1,98 @@
 package day2;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
 /**
+ * TODO when calculating the ribbon length needed, only use the two smallest numbers
+ * TODO currently the first two numbers in the array are bing used causing the answer to be too big
+ * <p>
+ * TODO update the documentation on calculatePaperNeeded(String e) to reflect the above need
+ * <p>
  * Created by murad on 11/02/16.
  */
 public class Day2 {
+
+    public static void main(String[] args) {
+        new Day2();
+    }
+
+    public Day2() {
+        System.out.println(readFile());
+    }
+
+    public String[] splitDataOnLine(String dataLine) {
+        String[] test = dataLine.split("x");
+        return test;
+    }
+
+    /**
+     * equation - 2*l*w + 2*w*h + 2*h*l
+     * example -
+     * 2x3x4
+     * 2*6 + 2*12 + 2*8 = 52
+     *
+     * @param dimensions
+     * @return
+     */
+    public int calculatePaperNeeded(String[] dimensions) {
+        int length = Integer.parseInt(dimensions[0]);
+        int width = Integer.parseInt(dimensions[1]);
+        int height = Integer.parseInt(dimensions[2]);
+
+        int squareFeet = (2 * length * width) + (2 * width * height) + (2 * height * length);
+        int[] twoSmallestSidesLengthForBow = min(width, length, height);
+        return squareFeet + twoSmallestSidesLengthForBow[0] * twoSmallestSidesLengthForBow[1];
+    }
+
+    /**
+     * finds the two smallest numbers out of the dimensions given
+     * used to find the length of ribbon needed for bow
+     * http://www.codinghelmet.com/?path=exercises/two-smallest
+     *
+     * @param
+     * @return
+     */
+    public int[] min(int width, int length, int height) {
+        int[] twoSmallestDimensions = new int[2];
+        int lowest = width;
+        int secondLowest = length;
+
+        if (length < width) {
+            lowest = length;
+            secondLowest = width;
+        }
+
+        if (height < lowest) {
+            secondLowest = lowest;
+            lowest = height;
+        } else if (height < secondLowest) {
+            secondLowest = height;
+        }
+
+        twoSmallestDimensions[0] = lowest;
+        twoSmallestDimensions[1] = secondLowest;
+        return twoSmallestDimensions;
+    }
+
+    public int readFile() {
+        int totalPaperNeeded = 0;
+        try {
+            FileReader fileReader = new FileReader("puzzel_inputs/Day2Input");
+            BufferedReader bufferedReader = new BufferedReader(fileReader);
+
+            String line;
+            String[] splitDimensions;
+
+            while ((line = bufferedReader.readLine()) != null) {
+                splitDimensions = splitDataOnLine(line);
+                totalPaperNeeded += calculatePaperNeeded(splitDimensions);
+                //System.out.println(Arrays.toString(splitDimensions)); //prints array
+            }
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+        return totalPaperNeeded;
+    }
 }
---
+++ src/day2/Day2Test.java
@@ -0,0 +1,53 @@
+package day2;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by murad-1 on 11/02/16.
+ */
+public class Day2Test {
+    Day2 day2;
+    @Before
+    public void setUp() throws Exception {
+         day2 = new Day2();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testSplitInput() throws Exception {
+        String[] expected = {"55","66", "77"};
+        String[] actual = day2.splitDataOnLine("55x66x77");
+        assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void testCalculate() throws Exception {
+        String [] boxDimensions = {"2", "3", "4"};
+        int actual = day2.calculatePaperNeeded(boxDimensions);
+        assertEquals(52, actual);
+    }
+
+    @Test
+    public void testReadAndCalculateAllBoxes() throws Exception {
+
+    }
+
+    @Test
+    public void testFindMinDimensions() throws Exception{
+        int expected = 6;
+        int[] minArray = day2.min(2,3,4);
+        int actual = minArray[0] * minArray[1];
+        System.out.println(Arrays.toString(minArray));
+                assertEquals(expected, actual);
+    }
+}

be83d655a8ee9de46f85e2f591e05c6f22ac619a
Murad Karasu <muradkarasu@gmail.com>
2016-02-12 13:46:16 +0000

    test

--- src/day2/Day2Test.java
+++ src/day2/Day2Test.java
@@ -34,7 +34,7 @@ public class Day2Test {
     public void testCalculate() throws Exception {
         String [] boxDimensions = {"2", "3", "4"};
         int actual = day2.calculatePaperNeeded(boxDimensions);
-        assertEquals(58, actual);
+        assertEquals(5000, actual);
     }
 
     @Test
